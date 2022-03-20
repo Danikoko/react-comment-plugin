2332afdb9dc414ba85ff7890d9934eb07ae132df
Daniel Enamudu <denamudu@bariansystems.com>
2022-03-20 18:45:03 +0100

    Third commit

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,4 +1,54 @@
-const comment = ({content}) => {
+import React, { useMemo } from 'react';
+function Comment({content, deleteComment, user}) {
+    
+    const timeStamp = useMemo(() => {
+        let date = Date.parse(content.created_at);
+        var seconds = Math.floor((new Date() - date) / 1000);
+    
+        var interval = seconds / 31536000;
+    
+        if (interval > 1) {
+          return Math.floor(interval) != 1
+            ? Math.floor(interval) + " years"
+            : Math.floor(interval) + " year";
+        }
+        interval = seconds / 2592000;
+        if (interval > 1) {
+          return Math.floor(interval) != 1
+            ? Math.floor(interval) + " months"
+            : Math.floor(interval) + " month";
+        }
+        interval = seconds / 86400;
+        if (interval > 1) {
+          return Math.floor(interval) != 1
+            ? Math.floor(interval) + " days"
+            : Math.floor(interval) + " day";
+        }
+        interval = seconds / 3600;
+        if (interval > 1) {
+          return Math.floor(interval) != 1
+            ? Math.floor(interval) + " hours"
+            : Math.floor(interval) + " hour";
+        }
+        interval = seconds / 60;
+        if (interval > 1) {
+          return Math.floor(interval) != 1
+            ? Math.floor(interval) + " minutes"
+            : Math.floor(interval) + " minute";
+        }
+        return Math.floor(seconds) != 1
+          ? Math.floor(seconds) + " seconds"
+          : Math.floor(seconds) + " second";
+    }, [content]);
+
+    const sevenDaysActive = useMemo(() => {
+        if ((new Date().getTime() - new Date(content.created_at).getTime()) >= 604800000) {
+            return true;
+        } else {
+            return false
+        }
+    }, [content])
+
     return (
         <div>
             <div className="top mt-2">
@@ -6,14 +56,24 @@ const comment = ({content}) => {
                     <div className="d-flex">
                         <div className="profile"></div>
                         <div>
-                            <span>Jason</span><br />
-                            <span>2 month ago</span>
+                            <span>
+                                {
+                                    user.id == content.user_id ? 'You' : 'Anon.'
+                                }
+                            </span><br />
+                            <span>{timeStamp} ago</span>
                         </div>
                     </div>
-                    <div className="dropdowndot">
-                        <i className="fa-solid fa-ellipsis dropbtndot"></i>
-                        <span className="dropdown-contentdot">Report this response</span>
-                    </div>
+                    {
+                        user.id == content.user_id
+                        ?
+                        <div className="dropdowndot">
+                            <i className="fa-solid fa-ellipsis dropbtndot"></i>
+                            <span onClick={() => deleteComment(content.id)} className="dropdown-contentdot">Delete</span>
+                        </div>
+                        :
+                        null
+                    }
                 </div>
             </div><br />
             <div>
@@ -37,4 +97,4 @@ const comment = ({content}) => {
     );
 }
 
-export default comment;
+export default Comment;
---
+++ src/pages/Login.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+function Login({setUser}) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginText, setLoginText] = useState('Login');
+
+    const handleEmailInput = e => setEmail(e.target.value);
+    const handlePasswordInput = e => setPassword(e.target.value);
+
+    const handleLogin = () => {
+        setLoginText('...');
+        axios.post('https://tests.tee-solutions.com/api/login', {
+            email,
+            password
+        })
+        .then(response => {
+            if (response.data.error) {
+                alert(response.data.error);
+            } else {
+                localStorage.setItem('user', JSON.stringify(response.data))
+                setUser(JSON.parse(localStorage.getItem('user')));
+                console.log(response);
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
+        .then(() => {
+            setLoginText('Login');
+        })
+    }
+
+    return (
+        <div className="container pt-5">
+            <h2>Login Form</h2>
+            <form className="mt-2">
+                <div className="form-group">
+                    <input value={email} style={ {width:'350px',border:'none'} } onChange={handleEmailInput} autoComplete="off"
+                        className="form-control mb-2" placeholder="Share your thought" id="email"
+                        required />
+                </div>
+                <div className="form-group">
+                    <input value={password} style={ {width:'350px',border:'none'} } onChange={handlePasswordInput} autoComplete="off"
+                        className="form-control mb-2" type="password" placeholder="Share your thought" id="password"
+                        required />
+                </div>
+                <div className="send" id="send">
+                    <div style={ { width: '100%', display: 'block' } } className="btn">
+                        <button style={ { width: '100%' } } onClick={email.length >= 3 && password.length ? handleLogin : null} type="button" className="send-btn">{loginText}</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
